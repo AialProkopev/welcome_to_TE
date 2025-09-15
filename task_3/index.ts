8bae9b33136130b8f7448135815e44c2f95200a7
Ayal Prokopev <freezzforever@gmail.com>
2025-09-15 13:14:47 +0300

    task 3

--- task_3/index.ts
+++ task_3/index.ts
@@ -1,6 +1,6 @@
 interface BallonI {
-    id: number
-    isPublic: boolean
+  id: number;
+  isPublic: boolean;
 }
 
 /**
@@ -9,39 +9,53 @@ interface BallonI {
  * @returns {Number} количество шариков
  * @example const res = await fetchBallonAmount(202);
  */
-async function fetchBallonAmount(id: BallonI['id']): Promise<number> {
-	const RANDOM_TIMEOUT: number = Math.ceil(Math.random() * 10000); // 1-9 секунд
-	const RANDOM_AMOUNT: number = Math.ceil(Math.random() * id); // случайное число
+async function fetchBallonAmount(id: BallonI["id"]): Promise<number> {
+  const RANDOM_TIMEOUT: number = Math.ceil(Math.random() * 10000); // 1-9 секунд
+  const RANDOM_AMOUNT: number = Math.ceil(Math.random() * id); // случайное число
 
-	return new Promise(resolve => setTimeout(() => resolve(RANDOM_AMOUNT), RANDOM_TIMEOUT));
+  return new Promise((resolve) =>
+    setTimeout(() => resolve(RANDOM_AMOUNT), RANDOM_TIMEOUT)
+  );
 }
 
 // данные о шариках
 const BALLONS: { [key: string]: BallonI } = {
-	red: {
-		id: 202,
-		isPublic: true,
-	},
-	blue: {
-		id: 356,
-		isPublic: false,
-	},
-	yellow: {
-		id: 451,
-		isPublic: false,
-	},
-	black: {
-		id: 35,
-		isPublic: true,
-	},
-	green: {
-		id: 191,
-		isPublic: true,
-	},
-	white: {
-		id: 911,
-		isPublic: true,
-	},
+  red: {
+    id: 202,
+    isPublic: true,
+  },
+  blue: {
+    id: 356,
+    isPublic: false,
+  },
+  yellow: {
+    id: 451,
+    isPublic: false,
+  },
+  black: {
+    id: 35,
+    isPublic: true,
+  },
+  green: {
+    id: 191,
+    isPublic: true,
+  },
+  white: {
+    id: 911,
+    isPublic: true,
+  },
 };
 
 // Ваш код здесь
+
+async function getPublicBallons() {
+  const publicBallons = Object.values(BALLONS)
+    .filter((ballon) => ballon.isPublic)
+    .map((ballon) => ballon.id);
+
+  const amounts = await Promise.all(
+    publicBallons.map((id) => fetchBallonAmount(id))
+  );
+
+  return amounts.reduce((sum, amount) => sum + amount, 0);
+}
